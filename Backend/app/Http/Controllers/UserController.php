68f2ad66a59303b380ae1278aa6ebab80c7d7015
Basudev-Pokharel <basudevpokharelfin@gmail.com>
2026-02-20 18:59:48 +0200

    feat: add API's for auth

---
+++ Backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
+        ]);
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validate->errors()
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        if ($user) {
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+            ], 200);
+        }
+    }
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validate->errors()
+            ], 422);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'user' => $user,
+                'token' => $user->createToken('user_token')->plainTextToken,
+            ], 200);
+        }
+    }
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->tokens()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'User Logged out Successfully',
+        ], 200);
+    }
+}
